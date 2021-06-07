ca066447482368e18f7dcbe3df951a666a67449e
Richard Barnes <rbarnes@umn.edu>
2021-06-07 10:04:44 -0600

    Use high resolution timer for timing

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -1,6 +1,6 @@
 /*
  * teexGraph --- by Frank Takes --- https://github.com/franktakes/teexgraph
- * 
+ *
  * The Timer class measures wall clock execution time, respecting paralellism.
  * Create the object Timer T and run T.click() to see time since the last click.
  */
@@ -10,28 +10,24 @@
 #include <iostream>
 using namespace std;
 
-// constructor
-Timer::Timer() {
-    current = -1;
-}
-
 // show and restart timer
 void Timer::click() {
-    if(current != -1)
-        cerr << "Time passed: " << now() - current << " sec." << endl;
+    if(used){
+        using namespace std::chrono;
+        const auto current_time = now();
+        const auto time_diff = duration_cast<duration<double>>((current_time-current));
+        cerr << "Time passed: " << time_diff.count() << " sec." << endl;
+    }
+    used = true;
     restart();
 }
 
 // get now time
-double Timer::now() {
-    #ifdef _OPENMP
-    return omp_get_wtime();
-    #endif
-    return(double) clock() / (double) CLOCKS_PER_SEC;
+std::chrono::high_resolution_clock::time_point Timer::now() {
+    return std::chrono::high_resolution_clock::now();
 }
 
 // restart timer
 void Timer::restart() {
     current = now();
 }
-
